998563a400c24cb0e629b3697914d7a008d2c248
Ameenijaz <ameenijaz142@gmail.com>
2024-11-10 20:46:13 +0500

    refactor: make updates

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const path = require("path");
 
 /** @type {import('next').NextConfig} */
-
 module.exports = {
   trailingSlash: true,
   reactStrictMode: false,
@@ -12,12 +11,25 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "http://localhost:3001" }, // Allow requests from localhost:3001
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        ],
+      },
+    ];
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       apexcharts: path.resolve(
         __dirname,
-        "./node_modules/apexcharts-clevision",
+        "./node_modules/apexcharts-clevision"
       ),
     };
 
--- src/pages/api/get-payment-methods.js
+++ src/pages/api/get-payment-methods.js
@@ -1,16 +1,24 @@
 import Payment from "../models/payment-methods";
 
-
 export default async function handler(req, res) {
-    if (req.method === "GET") {
-        console.log("hit get payment methods ??");
-        const { restaurant_id } = req.query;
-        try {
-            const methods = await Payment.find({ restaurant_id: restaurant_id });
-            res.status(200).json(methods);
-
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  
+    if (req.method === "OPTIONS") {
+      res.status(200).end();
+      return;
     }
-}
+  
+    if (req.method === "GET") {
+      console.log("hit get payment methods ??");
+      const { restaurant_id } = req.query;
+      try {
+        const methods = await Payment.find({ restaurant_id: restaurant_id });
+        res.status(200).json(methods);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    }
+  }
