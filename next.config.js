0bc529ab75bb56c1350bcd8373de9af4c586e19e
Ameenijaz <ameenijaz142@gmail.com>
2024-11-25 01:01:15 +0500

    refactor: refactor make some changes

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ module.exports = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:3001" }, // Allow requests from localhost:3001
+          { key: "Access-Control-Allow-Origin", value: "*"},
           { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
         ],
--- src/pages/api/get-payment-methods.js
+++ src/pages/api/get-payment-methods.js
@@ -1,24 +1,31 @@
 import Payment from "../models/payment-methods";
 
 export default async function handler(req, res) {
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  const allowedOrigins = [
+    "https://dine-qr-customer.vercel.app",
+    "http://dine-qr-customer.vercel.app"
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
   
-    if (req.method === "OPTIONS") {
-      res.status(200).end();
-      return;
-    }
-  
-    if (req.method === "GET") {
-      console.log("hit get payment methods ??");
-      const { restaurant_id } = req.query;
-      try {
-        const methods = await Payment.find({ restaurant_id: restaurant_id });
-        res.status(200).json(methods);
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
+  if (req.method === "GET") {
+    console.log("hit get payment methods ??");
+    const { restaurant_id } = req.query;
+    try {
+      const methods = await Payment.find({ restaurant_id: restaurant_id });
+      res.status(200).json(methods);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
   }
+}
